a47d340a3c84d0316dd99430e163874fce7761fc
fengluyao <1632174062@qq.com>
2018-11-08 20:04:16 +0800

    update lastTime

--- ygh_work/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql=require("mysql");
-var pool=mysql.createPool({
-    host:'127.0.0.1',
-    user:'root',
-    password:'',
-    database:'construction',
-    connectionLimit:10
-});
-module.exports=pool;
